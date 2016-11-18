e60d435fdc0ec739da35090f000e35e54f1cc98e
theGreatWhiteShark <thetruephil@googlemail.com>
2016-11-18 13:25:44 +0100

    working on the animation. so this will probably break some parts

---
+++ inst/climex_app/js/template2.js
@@ -0,0 +1,36 @@
+(function($) {
+    $('#animationLocSc').scianimator({
+	'images': [%imgLocSc],
+	'width': %width,
+	'delay': %delay,
+	'controls': 'NONE',
+	'loopMode': '%loop'
+    });
+    $('#animationLocSh').scianimator({
+	'images': [%imgLocSh],
+	'width': %width,
+	'delay': %delay,
+	'control': [ 'first', 'play', 'last' ],
+	'loopMode': '%loop'
+    });
+    $('#animationScSh').scianimator({
+	'images': [%imgScSh],
+	'width': %width,
+	'delay': %delay,
+	'controls': 'NONE',
+	'loopMode': '%loop'
+    });
+    $('#animationLocSc').scianimator('play');    
+    $('#animationLocSh').scianimator('play');
+    $('#animationScSh').scianimator('play');
+
+    $('#animationLocSh-play').bind( 'click', function( event ){
+	if ( $('#animationLocSh-play').attr( 'class' ).match( 'play' ) ){
+	    $('#animationLocSc').scianimator( 'play' );
+	    $('#animationScSh').scianimator( 'play' );
+	} else {
+	    $('#animationLocSc').scianimator( 'stop' );
+	    $('#animationScSh').scianimator( 'stop' );
+	}
+    } );
+})(jQuery);
