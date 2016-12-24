81118e38c46bdf8dfbaae49e8ad0c881c54f1380
shark <thetruephil@googlemail.com>
2016-12-24 20:37:50 +0100

    combined the two vignettes into one and update them

---
+++ inst/doc/climex_app.R
@@ -0,0 +1,5 @@
+## ------------------------------------------------------------------------
+CLIMEX.PATH <<- "~/R/climex/"
+if ( !dir.exists( CLIMEX.PATH ) )
+    dir.create( CLIMEX.PATH, recursive = TRUE )
+
