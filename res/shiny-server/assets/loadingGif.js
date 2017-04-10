207553ae6e6c2a15670263d7a13748d81dcea4c3
shark@tc08 <thetruephil@googlemail.com>
2017-04-10 22:04:05 +0200

    Updating shiny assets

--- res/shiny-server/assets/loadingGif.js
+++ res/shiny-server/assets/loadingGif.js
@@ -1,50 +1,37 @@
 (function($) {
-    /* if the animation is started, show the Gif */
-    $('#buttonDrawAnimation').bind( 'click', function( event ){
-	$( '#loadingGif' ).css( 'visibility', 'visible' );
-	$( '#loadingGif' ).css( 'width', '100%' );
+  /* if the animation is started, show the Gif */
+  $('#animation-buttonDrawAnimation').bind( 'click', function( event ){
+    $( '#busy-loadingGif' ).css( 'visibility', 'visible' );
+    $( '#busy-loadingGif' ).css( 'width', '100%' );
+  });
+  /* if the animation is started, show the Gif */
+  $('#leaflet-buttonDrawMarkers').bind( 'click', function( event ){
+    $( '#busy-loadingGif' ).css( 'visibility', 'visible' );
+    $( '#busy-loadingGif' ).css( 'width', '100%' );
+  });
+  /* since the shiny server should be idle after sending the pictures hide it*/
+  $(document).on( 'shiny:idle', function( event ){
+    $( '#busy-loadingGif' ).css( 'visibility', 'hidden' );
+    $( '#busy-loadingGif' ).css( 'width', '0%' );
+    // Transforming the scale of the return level slider in the leatlet map
+    // to a logarithmic scale. Since the element will be enabled and disabled
+    // depending on the choice of options, it's done on every idle
+    // Check for element to exists
+    if ( $( '#sidebarSeriesLength' ).length ){
+      // Check if element is just the placeholder div or the real deal
+      if ( $( '#sidebarSeriesLength' ).contents().length > 0 ){
+	$( '#sliderSeriesLength' ).data( 'ionRangeSlider' ).update({
+	  'prettify': function( exponent ) { return( Math.round( Math.pow( 10, exponent ) ) ); }
+	});
+      }
+    }
+  });
+  // Transforming the scale of the return level slider in the leatlet map
+  // to a logarithmic scale.
+  // Check for element to exists
+  if ( $( '#leaflet-sliderReturnLevel' ).length ){
+    $( '#leaflet-sliderReturnLevel' ).data( 'ionRangeSlider' ).update({
+      'prettify': function( exponent ) { return( Math.round( Math.pow( 10, exponent ) ) ); }
     });
-    /* if the animation is started, show the Gif */
-    $('#buttonDrawMarkers').bind( 'click', function( event ){
-	$( '#loadingGif' ).css( 'visibility', 'visible' );
-	$( '#loadingGif' ).css( 'width', '100%' );
-    });
-    /* since the shiny server should be idle after sending the pictures hide it*/
-    $(document).on( 'shiny:idle', function( event ){
-	$( '#loadingGif' ).css( 'visibility', 'hidden' );
-	$( '#loadingGif' ).css( 'width', '0%' );
-	
-	/* I want to display the leaflet legend  horizontally */
-	/* So whenever shiny has nothing to do check if the legend was already created */
-	// if ( $( ".leaflet-right > .legend > div > span" ).length ) {
-	//     gradientString = $( ".leaflet-right > .legend > div > span" ).css( "background-image" );
-	//     gradientString2 = gradientString.replace( "gradient(", "gradient( to right," );
-	//     console.log( "1" );
-	//     $( ".leaflet-right > .legend > div > span" ).css( "background-image", gradientString2 );
-	// };
-    });
-    /* since the shiny server should be idle after sending the pictures hide it*/
-    $(document).on( 'shiny:visualchange', function( event ){
-    	/* I want to display the leaflet legend  horizontally */
-    	/* So whenever shiny has nothing to do check if the legend was already created */
-    	console.log( "shiny:visualchange" );
-    	if ( $( ".leaflet-right > .legend > div > span" ).length ) {
-    	    gradientString = $( ".leaflet-right > .legend > div > span" ).css( "background-image" );
-    	    gradientString2 = gradientString.replace( "gradient(", "gradient( to right," );
-    	    console.log( "shiny:visualchange2" );
-    	    $( ".leaflet-right > .legend > div > span" ).delay(4000).css( "background-image", gradientString2 );
-    	};
-    });
-    $(document).on( "shiny:idle", function( event ){
-    	/* I want to display the leaflet legend  horizontally */
-    	/* So whenever shiny has nothing to do check if the legend was already created */
-    	console.log( "shiny:idle1" );
-    	if ( $( ".leaflet-right > .legend > div > span" ).length ) {
-    	    gradientString = $( ".leaflet-right > .legend > div > span" ).css( "background-image" );
-    	    gradientString2 = gradientString.replace( "gradient(", "gradient( to right," );
-    	    console.log( "shiny:idle2" );
-    	    $( ".leaflet-right > .legend > div > span" ).delay(4000).css( "background-image", gradientString2 );
-    	    console.log( "shiny:idle3" );
-    	};
-    });
+  }
 })(jQuery);
